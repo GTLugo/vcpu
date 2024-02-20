4bca040eedc030ac8983f265854966dc5856fddf
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-02-20 18:32:04 -0500

    Updated assembly file

--- assets/main.asm
+++ assets/main.asm
@@ -1,2 +1,24 @@
-  lda #$ff
-  sta $6002
+    .org $0000
+ram_begin:
+    
+    .org $8000
+    
+reset:
+    lda #$ff
+    sta $6002
+
+loop:
+    lda #$55
+    sta $6000
+
+    lda #$aa
+    sta $6000
+
+    jmp loop
+
+    .org $fffc
+  
+ram_end:
+    .word reset
+    .word $0000
+    
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use std::io::stdin;
 use vcpu::core::cpu::Cpu;
-use vcpu::core::bus::BusItem;
 use vcpu::core::ram_file::RamFile;
 
 fn main() {
   let mut ram = RamFile::new(0x0000..=0xFFFF);
-  ram.write_all(0x8000, include_bytes!("../assets/a.out"));
-  ram.write(0xFFFC, 0x00);
-  ram.write(0xFFFD, 0x80);
+  ram.write_all(0x0000, include_bytes!("../assets/a.out"));
 
-  let mut cpu = Cpu::new(8.0);
+  let mut cpu = Cpu::new(100.0);
   cpu.connect(ram);
 
   cpu.reset();
